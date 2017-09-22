0590b113d44cc288dde620c98b377c846b412197
zhangchangchao <zcc225@163.com>
2017-09-22 18:29:00 +0800

    初始导入

---
+++ src/main/java/com/zccpro/mapper/TAccountsLogsMapper.java
@@ -0,0 +1,8 @@
+package com.zccpro.mapper;
+
+import com.zccpro.model.TAccountsLogs;
+import com.zccpro.util.MyMapper;
+
+
+public interface TAccountsLogsMapper extends MyMapper<TAccountsLogs> {
+}
---
+++ src/main/java/com/zccpro/service/TAccountsLogsServer.java
@@ -0,0 +1,6 @@
+package com.zccpro.service;
+
+import com.zccpro.model.TAccountsLogs;
+
+public interface TAccountsLogsServer extends IService<TAccountsLogs> {
+}
